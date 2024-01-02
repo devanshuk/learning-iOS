c5ce91675cdd942502ee4283b1db10ec1656f616
devanshu <bits.devanshu@gmail.com>
2024-01-02 18:18:41 +0530

    DICEE: first IBOutlet

--- Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageViewOne.image = UIImage(named: "DiceSix")
     }
 
 
