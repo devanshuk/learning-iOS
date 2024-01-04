26724fe1f7b1e372cad35fcb146d279ccbd3cf14
devanshu <bits.devanshu@gmail.com>
2024-01-05 01:13:37 +0530

    EGG: added countdown timer

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let boilTimeDict = ["Soft" : 5, "Medium": 8, "Hard": 12]
+    let boilTimeDict = ["Soft" : 5*60, "Medium": 8*60, "Hard": 12*60]
+    var timer: Timer?
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        resetTimer()
         if let selectedHardness = sender.currentTitle {
-            if let boilTime = boilTimeDict[selectedHardness] {
-                print("You want your eggs \(selectedHardness), boil for \(boilTime) mins")
+            startTimer(for: selectedHardness)
+        }
+    }
+    
+    func startTimer(for selectedHardness: String) {
+        if let boilTime = boilTimeDict[selectedHardness] {
+            print("You want your eggs \(selectedHardness), boil for:")
+            timer = countDown(from: boilTime) { secondsRemaining in
+                print("\(secondsRemaining) seconds.")
+            }
+        }
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+    }
+    
+    func countDown(from value: Int, block: @escaping (Int) -> Void) -> Timer {
+        var secondsRemaining = value
+        return Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+            if secondsRemaining > 0 {
+                block(secondsRemaining)
+                secondsRemaining -= 1
+            } else {
+                timer.invalidate()
             }
         }
     }
