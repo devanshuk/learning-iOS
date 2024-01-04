bd9814eca6180d6b0389a5afb176cbbdbb1a6260
devanshu <bits.devanshu@gmail.com>
2024-01-05 01:48:19 +0530

    EGG: timer progress shown with progressView

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
     
     let boilTimeDict = ["Soft" : 5/*5*60*/, "Medium": 8/*8*60*/, "Hard": 12/*12*60*/]
     var timer: Timer?
+    var selectedHardness: String?
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         resetTimer()
         if let selectedHardness = sender.currentTitle {
+            self.selectedHardness = selectedHardness
             startTimer(for: selectedHardness)
         }
     }
@@ -26,16 +29,37 @@ class ViewController: UIViewController {
         if let boilTime = boilTimeDict[selectedHardness] {
             print("You want your eggs \(selectedHardness), boil for:")
             timer = countDown(from: boilTime) { secondsRemaining in
-                print("\(secondsRemaining) seconds.")
+                self.updateProgress(secondsRemaining: secondsRemaining)
             }
         }
     }
     
     func resetTimer() {
         timer?.invalidate()
+        progressView.setProgress(0.0, animated: false)
     }
     
-    func countDown(from value: Int, block: @escaping (Int) -> Void) -> Timer {
+    func updateProgress(secondsRemaining: Int) {
+        print("\(secondsRemaining) seconds.")
+        
+        if let hardness = selectedHardness {
+            if let boilTime = boilTimeDict[hardness] {
+                progressView
+                    .setProgress(
+                        Float(
+                            boilTime-secondsRemaining
+                        )/Float(boilTime),
+                        animated: true
+                    )
+            }
+        }
+        
+    }
+    
+    func countDown(
+        from value: Int,
+        block: @escaping (Int) -> Void
+    ) -> Timer {
         var secondsRemaining = value
         return Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
             if secondsRemaining > 0 {
@@ -43,10 +67,11 @@ class ViewController: UIViewController {
                 secondsRemaining -= 1
             } else {
                 timer.invalidate()
+                self.progressView.setProgress(1.0, animated: true)
                 self.titleLabel.text = "Done"
             }
         }
     }
     
-
+    
 }
