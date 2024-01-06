6f87faa3bdc314f7445076b6795620fd3fe019ef
devanshu <bits.devanshu@gmail.com>
2024-01-06 23:45:17 +0530

    QUIZ: cylcled questions on button press

--- Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Four + Two equals Six",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        progressBar.progress = 0.0
+        loadQuestionNumber(0)
     }
-
+    
+    func loadQuestionNumber(_ num: Int) {
+        questionLabel.text = quiz[num]
+    }
+    
+    func nextQuestion() {
+        progressBar.progress = Float(questionNumber+1)/Float(quiz.count)
+        if(questionNumber < quiz.count - 1) {
+            questionNumber += 1
+            loadQuestionNumber(questionNumber)
+        }
+    }
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        nextQuestion()
+    }
+    
 
 }
 
